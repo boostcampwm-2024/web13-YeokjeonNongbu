d4c04f82b4156d459cce167c3b1d45c5cfa86ac4
edder773 <st02045@naver.com>
2024-12-03 14:49:49 +0900

    [test] 배포 테스트

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -7,7 +7,11 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
-    cors: true
+    cors: true,
+    hmr: {
+      host: 'yeokjeonnongbu.shop',
+      port: 3000
+    }
   },
 
   resolve: {
