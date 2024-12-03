318ec60cd485ac750e378c593418995a38e65720
edder773 <st02045@naver.com>
2024-12-03 10:58:26 +0900

    [test] 배포 테스트

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 
-// https://vite.dev/config
+// https://vite.dev/conf
 export default defineConfig({
   plugins: [react()],
   server: {
